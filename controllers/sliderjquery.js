22e3fae384020c3707c2030123f926f99a557df8
majd ewawi <majd199ziad@gmail.com>
2020-02-28 17:54:39 +0200

    edit slider functionality

---
+++ controllers/sliderjquery.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    let counter = 1;
+    const imageWidth = $('.slide img').width();
+
+    $('#prevBtn').click(() => {
+        if (counter <= 0) return;
+        $('.slide').css('transition', 'transform 0.4s ease-in-out');
+        counter--;
+        $('.slide').css('transform', `translateX(${-imageWidth * counter}px)`);
+    });
+
+    $('#nextBtn').click(() => {
+        if (counter >= $('.slide img').length - 1) return;
+        $('.slide').css('transition', 'transform 0.4s ease-in-out');
+        counter++;
+        $('.slide').css('transform', `translateX(${-imageWidth * counter}px)`);
+    });
+
+    $('.slide').on('transitionend', () => {
+        if ($('.slide img')[counter].id == 'lastImage') {
+            $('.slide').css('transition', 'none');
+            counter = $('.slide img').length - 2;
+            $('.slide').css('transform', `translateX(${-imageWidth * counter}px)`);
+        }
+        else if ($('.slide img')[counter].id == 'firstImage') {
+            $('.slide').css('transition', 'none');
+            counter = $('.slide img').length - counter;
+            $('.slide').css('transform', `translateX(${-imageWidth * counter}px)`);
+        }
+    });
+});
